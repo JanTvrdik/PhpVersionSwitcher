99a2bc7bba5d623b4b1052a9c43c2c2bece5e5a4
Jan Tvrdik <jan.tvrdik@gmail.com>
2014-08-01 23:51:06 +0200

    Version: resharper cleanup

--- src/app/Version.cs
+++ src/app/Version.cs
@@ -4,47 +4,47 @@ using System.Text.RegularExpressions;
 
 namespace PhpVersionSwitcher
 {
-	public class Version : IComparable<Version>
+	public class Version : IComparable<Version>, IEquatable<Version>
 	{
-		public enum VersionStability { Alpha = 0, Beta = 1, RC = 2, Stable = 3 };
-
-		public int Major
+		public enum VersionStability
 		{
-			get;
-			private set;
-		}
-
-		public int Minor
-		{
-			get;
-			private set;
-		}
-
-		public int Patch
-		{
-			get;
-			private set;
-		}
-
-		public VersionStability Stability
-		{
-			get;
-			private set;
-		}
-
-		public int StabilityVersion
-		{
-			get;
-			private set;
-		}
+			Alpha = 0,
+			Beta = 1,
+			RC = 2,
+			Stable = 3
+		};
 
 		public Version(int major, int minor, int patch, VersionStability stability, int stabilityVersion)
 		{
-			Major = major;
-			Minor = minor;
-			Patch = patch;
-			Stability = stability;
-			StabilityVersion = stabilityVersion;
+			this.Major = major;
+			this.Minor = minor;
+			this.Patch = patch;
+			this.Stability = stability;
+			this.StabilityVersion = stabilityVersion;
+		}
+
+		public int Major { get; private set; }
+
+		public int Minor { get; private set; }
+
+		public int Patch { get; private set; }
+
+		public VersionStability Stability { get; private set; }
+
+		public int StabilityVersion { get; private set; }
+
+		public int CompareTo(Version other)
+		{
+			if (this.Major != other.Major) return this.Major - other.Major;
+			if (this.Minor != other.Minor) return this.Minor - other.Minor;
+			if (this.Patch != other.Patch) return this.Patch - other.Patch;
+			if (this.Stability != other.Stability) return this.Stability - other.Stability;
+			return this.StabilityVersion - other.StabilityVersion;
+		}
+
+		public bool Equals(Version other)
+		{
+			return CompareTo(other) == 0;
 		}
 
 		public static bool TryParse(string s, out Version version)
@@ -56,34 +56,20 @@ namespace PhpVersionSwitcher
 				return false;
 			}
 
-			int major = Int32.Parse(match.Groups[1].Value);
-			int minor = Int32.Parse(match.Groups[2].Value);
-			int patch = Int32.Parse(match.Groups[3].Value);
+			var major = Int32.Parse(match.Groups[1].Value);
+			var minor = Int32.Parse(match.Groups[2].Value);
+			var patch = Int32.Parse(match.Groups[3].Value);
 			var stability = VersionStability.Stable;
-			int stabilityVersion = 0;
+			var stabilityVersion = 0;
 
 			if (match.Groups[4].Success)
 			{
-				stability = (VersionStability)Enum.Parse(typeof(VersionStability), match.Groups[4].Value, true);
+				stability = (VersionStability) Enum.Parse(typeof (VersionStability), match.Groups[4].Value, true);
 				stabilityVersion = Int32.Parse(match.Groups[5].Value);
 			}
 
 			version = new Version(major, minor, patch, stability, stabilityVersion);
 			return true;
-		}
-
-		public bool Equals(Version other)
-		{
-			return this.CompareTo(other) == 0;
-		}
-
-		public int CompareTo(Version other)
-		{
-			if (this.Major != other.Major) return this.Major - other.Major;
-			else if (this.Minor != other.Minor) return this.Minor - other.Minor;
-			else if (this.Patch != other.Patch) return this.Patch - other.Patch;
-			else if (this.Stability != other.Stability) return this.Stability - other.Stability;
-			else return this.StabilityVersion - other.StabilityVersion;
 		}
 
 		public override string ToString()
