16e08f96080889b9c9d848723699f82295ab6fb8
Jan Tvrdik <jan.tvrdik@gmail.com>
2014-08-03 13:04:41 +0200

    added ProcessManager

---
+++ src/app/ProcessManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PhpVersionSwitcher
+{
+	class ProcessManager : IProcessManager
+	{
+		public string WorkingDirectory { get; private set; }
+		public string FileName { get; private set; }
+		public string Arguments { get; private set; }
+		public string ProcessName { get { return this.FileName.Replace(".exe", ""); }}
+
+		public ProcessManager(string workingDirectory, string fileName, string arguments)
+		{
+			this.WorkingDirectory = workingDirectory;
+			this.FileName = fileName;
+			this.Arguments = arguments;
+		}
+
+		public bool IsRunning()
+		{
+			return this.GetProcess() != null;
+		}
+
+		public Task Start()
+		{
+			return Task.Run(() =>
+			{
+				try
+				{
+					Process.Start(new ProcessStartInfo()
+					{
+						WorkingDirectory = this.WorkingDirectory,
+						FileName = this.FileName,
+						Arguments = this.Arguments,
+						CreateNoWindow = true,
+						WindowStyle = ProcessWindowStyle.Hidden,
+					});
+				}
+				catch
+				{
+					throw new ServiceStartFailedException(this.FileName);
+				}
+			});
+		}
+
+		public Task Stop()
+		{
+			return Task.Run(() =>
+			{
+				try
+				{
+					var process = this.GetProcess();
+					if (process == null) return;
+
+					process.Kill();
+					if (process.WaitForExit(7000)) return;
+				}
+				catch { }
+
+				throw new ServiceStopFailedException(this.FileName);
+			});
+		}
+
+		public async Task Restart()
+		{
+			await this.Stop();
+			await this.Start();
+		}
+
+		private Process GetProcess()
+		{
+			var processes = Process.GetProcessesByName(this.ProcessName);
+			return processes.Length > 0 ? processes[0] : null;
+		}
+	}
+}
