8af4d16ef3cb3f2146d46d770519b780a7f03818
Jan Tvrdik <jan.tvrdik@gmail.com>
2014-08-04 06:56:55 +0200

    refactoring

--- src/ui/MainForm.cs
+++ src/ui/MainForm.cs
@@ -8,14 +8,13 @@ namespace PhpVersionSwitcher
 {
 	public partial class MainForm : Form
 	{
-		private List<ProcessMenu> submenus;
+		private List<ProcessMenu> processMenus;
 		private WaitingForm waitingForm;
-		private ToolStripMenuItem activeVersionItem;
 		private VersionsManager phpVersions;
 
 		public MainForm()
 		{
-			this.submenus = new List<ProcessMenu>();
+			this.processMenus = new List<ProcessMenu>();
 			this.waitingForm = new WaitingForm();
 
 			IProcessManager server = null;
@@ -25,19 +24,19 @@ namespace PhpVersionSwitcher
 				if (Settings.Default.HttpServerServiceName.Trim().Length > 0)
 				{
 					server = new ServiceManager(Settings.Default.HttpServerServiceName);
-					this.submenus.Add(new ProcessMenu(this, server));
+					this.RegisterProcessManager(server);
 				}
 
 				if (Settings.Default.HttpServerProcessPath.Trim().Length > 0)
 				{
 					server = new ProcessManager(Settings.Default.HttpServerProcessPath);
-					this.submenus.Add(new ProcessMenu(this, server));
+					this.RegisterProcessManager(server);
 				}
 			
 				if (Settings.Default.FastCgiAddress.Trim().Length > 0)
 				{
 					server = new ProcessManager(Settings.Default.PhpDir + "\\active\\php-cgi.exe", "-b " + Settings.Default.FastCgiAddress);
-					this.submenus.Add(new ProcessMenu(this, server));
+					this.RegisterProcessManager(server);
 				}
 			}
 			catch (Exception ex)
@@ -58,43 +57,44 @@ namespace PhpVersionSwitcher
 
 		private void InitializeMainMenu()
 		{
+			this.notifyIconMenu.Items.Clear();
 			var activeVersion = this.phpVersions.GetActive();
 			var versions = this.phpVersions.GetAvailable();
 
-			this.notifyIconMenu.Items.Clear();
 			foreach (var version in versions)
 			{
-				var item = new ToolStripMenuItem(version.ToString(), null, this.version_Clicked);
-				item.Tag = version;
-
-				if (activeVersion != null && version.Equals(activeVersion))
+				var item = new ToolStripMenuItem(version);
+				item.Checked = version.Equals(activeVersion);
+				item.Click += (sender, args) => this.Attempt("PHP version to change", async () =>
 				{
-					this.SetActiveItem(item);
-				}
+					await this.phpVersions.SwitchTo(version);
+				});
 
 				this.notifyIconMenu.Items.Add(item);
 			}
 
 			this.notifyIconMenu.Items.Add(new ToolStripSeparator());
 
-			foreach (var menu in this.submenus)
+			foreach (var menu in this.processMenus)
 			{
+				menu.Refresh();
 				this.notifyIconMenu.Items.Add(menu);
 			}
 
-			this.notifyIconMenu.Items.Add(new ToolStripSeparator());
-			this.notifyIconMenu.Items.Add("Refresh", null, this.refresh_Clicked);
-			this.notifyIconMenu.Items.Add("Close", null, this.close_Click);
+			this.notifyIconMenu.Items.Add("Close", null, (sender, args) => Application.Exit());
 		}
 
-		private void SetActiveItem(ToolStripMenuItem item)
+		private void RegisterProcessManager(IProcessManager pm)
 		{
-			if (this.activeVersionItem != null) this.activeVersionItem.Checked = false;
-			this.activeVersionItem = item;
-			this.activeVersionItem.Checked = true;
+			var menu = new ProcessMenu(pm);
+			menu.StartItem.Click += (sender, args) => this.Attempt(pm.Name + " to start", pm.Start);
+			menu.StopItem.Click += (sender, args) => this.Attempt(pm.Name + " to stop", pm.Stop);
+			menu.RestartItem.Click += (sender, args) => this.Attempt(pm.Name + " to restart", pm.Restart);
+
+			this.processMenus.Add(menu);
 		}
 
-		public async void Attempt(string description, Func<Task> action)
+		private async void Attempt(string description, Func<Task> action)
 		{
 			this.notifyIconMenu.Enabled = false;
 			this.waitingForm.description.Text = @"Waiting for " + description + @"...";
@@ -109,49 +109,20 @@ namespace PhpVersionSwitcher
 				}
 				catch (ProcessException ex)
 				{
-					var dialogResult = MessageBox.Show("Unable to " + ex.Operation + " " + ex.Name + ".", "Operation failed", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+					var msg = "Unable to " + ex.Operation + " " + ex.Name + ".";
+					var dialogResult = MessageBox.Show(msg, "Operation failed", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
 					if (dialogResult != DialogResult.Retry) break;
 				}
 			}
 			
-			this.RefreshSubMenus();
+			this.InitializeMainMenu();
 			this.waitingForm.Hide();
 			this.notifyIconMenu.Enabled = true;
-		}
-
-		private void RefreshSubMenus()
-		{
-			foreach (var menu in this.submenus)
-			{
-				menu.Refresh();
-			}
 		}
 
 		private void ShowFatalError(string message)
 		{
 			MessageBox.Show(message, "Fatal error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-			Application.Exit();
-		}
-
-		private void version_Clicked(object sender, EventArgs e)
-		{
-			var menuItem = (ToolStripMenuItem) sender;
-			var version = (Version) menuItem.Tag;
-
-			this.Attempt("PHP version to change", async () =>
-			{
-				await this.phpVersions.SwitchTo(version);
-				this.SetActiveItem(menuItem);
-			});
-		}
-
-		private void refresh_Clicked(object sender, EventArgs e)
-		{
-			this.InitializeMainMenu();
-		}
-
-		private void close_Click(object sender, EventArgs e)
-		{
 			Application.Exit();
 		}
 	}
--- src/ui/ProcessMenu.cs
+++ src/ui/ProcessMenu.cs
@@ -1,52 +1,31 @@
-﻿using System;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 using PhpVersionSwitcher.Properties;
 
 namespace PhpVersionSwitcher
 {
 	class ProcessMenu : ToolStripMenuItem
 	{
-		private readonly MainForm form;
-		private readonly IProcessManager processManager;
+		public IProcessManager ProcessManager { get; private set; }
+		public ToolStripItem StartItem { get; private set; }
+		public ToolStripItem StopItem { get; private set; }
+		public ToolStripItem RestartItem { get; private set; }
 
-		private ToolStripItem startItem;
-		private ToolStripItem stopItem;
-		private ToolStripItem restartItem;
-
-		public ProcessMenu(MainForm form, IProcessManager processManager)
-			: base(processManager.Name)
+		public ProcessMenu(IProcessManager processManager) : base(processManager.Name)
 		{
-			this.form = form;
-			this.processManager = processManager;
-
-			this.startItem = this.DropDownItems.Add("Start", Resources.Start, this.startItem_Clicked);
-			this.stopItem = this.DropDownItems.Add("Stop", Resources.Stop, this.stopItem_Clicked);
-			this.restartItem = this.DropDownItems.Add("Restart", Resources.Restart, this.restartItem_Clicked);
+			this.ProcessManager = processManager;
+			this.StartItem = this.DropDownItems.Add("Start", Resources.Start);
+			this.StopItem = this.DropDownItems.Add("Stop", Resources.Stop);
+			this.RestartItem = this.DropDownItems.Add("Restart", Resources.Restart);
 			this.Refresh();
 		}
 
 		public void Refresh()
 		{
-			bool running = this.processManager.IsRunning();
+			bool running = this.ProcessManager.IsRunning();
 			this.Image = running ? Resources.Start : Resources.Stop;
-			this.startItem.Enabled = !running;
-			this.stopItem.Enabled = running;
-			this.restartItem.Enabled = running;
-		}
-
-		private void startItem_Clicked(object sender, EventArgs e)
-		{
-			this.form.Attempt("HTTP server to start", this.processManager.Start);
-		}
-
-		private void stopItem_Clicked(object sender, EventArgs e)
-		{
-			this.form.Attempt("HTTP server to stop", this.processManager.Stop);
-		}
-
-		private void restartItem_Clicked(object sender, EventArgs e)
-		{
-			this.form.Attempt("HTTP server to restart", this.processManager.Restart);
+			this.StartItem.Enabled = !running;
+			this.StopItem.Enabled = running;
+			this.RestartItem.Enabled = running;
 		}
 	}
 }
