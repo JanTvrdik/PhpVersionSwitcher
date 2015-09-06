fa05df0b165be368c9b85878fd634a938e712411
Jan Tvrdik <jan.tvrdik@gmail.com>
2015-09-06 12:11:48 +0200

    updated version to 1.5-dev

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PhpVersionSwitcher")]
-[assembly: AssemblyCopyright("Copyright © 2014 Jan Tvrdík")]
+[assembly: AssemblyCopyright("Copyright © 2015 Jan Tvrdík")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,4 +29,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.0.*")]
+[assembly: AssemblyVersion("1.5.0.*")]
