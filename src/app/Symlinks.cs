2e764e1780e1b6fa1acf8deeab29c8446fbbfc57
Jan Tvrdik <jan.tvrdik@gmail.com>
2016-04-24 11:51:15 +0200

    Symlinks: native methods moved to NativeMethods subclass
    (MS recommendation)

--- src/app/Symlinks.cs
+++ src/app/Symlinks.cs
@@ -12,25 +12,28 @@ namespace PhpVersionSwitcher
 		private const int FileFlagBackupSemantics = 0x02000000;
 		private const int SymbolicLinkFlagDirectory = 1;
 
-		// http://msdn.microsoft.com/en-us/library/aa364962(v=vs.85).aspx
-		[DllImport("kernel32.dll", EntryPoint = "GetFinalPathNameByHandleW", CharSet = CharSet.Unicode, SetLastError = true)]
-		private static extern int GetFinalPathNameByHandle(IntPtr handle, [In, Out] StringBuilder path, int bufLen, int flags);
+		internal static class NativeMethods
+		{
+			// http://msdn.microsoft.com/en-us/library/aa364962(v=vs.85).aspx
+			[DllImport("kernel32.dll", EntryPoint = "GetFinalPathNameByHandleW", CharSet = CharSet.Unicode, SetLastError = true)]
+			internal static extern int GetFinalPathNameByHandle(IntPtr handle, [In, Out] StringBuilder path, int bufLen, int flags);
 
-		// http://msdn.microsoft.com/en-us/library/aa363858(v=vs.85).aspx
-		[DllImport("kernel32.dll", EntryPoint = "CreateFileW", CharSet = CharSet.Unicode, SetLastError = true)]
-		private static extern SafeFileHandle CreateFile(string lpFileName, int dwDesiredAccess, int dwShareMode, IntPtr securityAttributes, int dwCreationDisposition, int dwFlagsAndAttributes, IntPtr hTemplateFile);
+			// http://msdn.microsoft.com/en-us/library/aa363858(v=vs.85).aspx
+			[DllImport("kernel32.dll", EntryPoint = "CreateFileW", CharSet = CharSet.Unicode, SetLastError = true)]
+			internal static extern SafeFileHandle CreateFile(string lpFileName, int dwDesiredAccess, int dwShareMode, IntPtr securityAttributes, int dwCreationDisposition, int dwFlagsAndAttributes, IntPtr hTemplateFile);
 
-		// http://msdn.microsoft.com/en-us/library/aa363866(v=vs.85).aspx
-		[DllImport("kernel32.dll", EntryPoint = "CreateSymbolicLinkW", CharSet = CharSet.Unicode, SetLastError = true)]
-		private static extern bool CreateSymbolicLink(string lpSymlinkFileName, string lpTargetFileName, int dwFlags);
+			// http://msdn.microsoft.com/en-us/library/aa363866(v=vs.85).aspx
+			[DllImport("kernel32.dll", EntryPoint = "CreateSymbolicLinkW", CharSet = CharSet.Unicode, SetLastError = true)]
+			internal static extern bool CreateSymbolicLink(string lpSymlinkFileName, string lpTargetFileName, int dwFlags);
+		}
 
 		public static string GetTarget(string symlink)
 		{
-			SafeFileHandle fileHandle = CreateFile(symlink, 0, 2, IntPtr.Zero, CreationDispositionOpenExisting, FileFlagBackupSemantics, IntPtr.Zero);
+			SafeFileHandle fileHandle = NativeMethods.CreateFile(symlink, 0, 2, IntPtr.Zero, CreationDispositionOpenExisting, FileFlagBackupSemantics, IntPtr.Zero);
 			if (fileHandle.IsInvalid) throw new Win32Exception(Marshal.GetLastWin32Error());
 
 			var path = new StringBuilder(512);
-			int size = GetFinalPathNameByHandle(fileHandle.DangerousGetHandle(), path, path.Capacity, 0);
+			int size = NativeMethods.GetFinalPathNameByHandle(fileHandle.DangerousGetHandle(), path, path.Capacity, 0);
 			if (size < 0) throw new Win32Exception(Marshal.GetLastWin32Error());
 
 			// The remarks section of GetFinalPathNameByHandle mentions the return being prefixed with "\\?\"
@@ -42,7 +45,7 @@ namespace PhpVersionSwitcher
 
 		public static bool CreateDir(string symlink, string target)
 		{
-			return CreateSymbolicLink(symlink, target, SymbolicLinkFlagDirectory);
+			return NativeMethods.CreateSymbolicLink(symlink, target, SymbolicLinkFlagDirectory);
 		}
 	}
 }
